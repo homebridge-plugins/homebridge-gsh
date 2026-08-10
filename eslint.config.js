9e06460ef7c7702bdd5e49e18ee350be79180cc2
Northern Man <19808920+NorthernMan54@users.noreply.github.com>
2026-08-10 10:29:18 -0400

    Lint working

---
+++ eslint.config.js
@@ -0,0 +1,62 @@
+import js from '@eslint/js';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+  {
+    ignores: [
+      'dist/**',
+      'homebridge-ui/**',
+      'chrome-profile/**',
+    ],
+  },
+
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+      },
+    },
+
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+
+    rules: {
+      ...js.configs.recommended.rules,
+      ...tsPlugin.configs.recommended.rules,
+
+      'quotes': ['warn', 'single'],
+      'indent': ['warn', 2, { SwitchCase: 1 }],
+      'semi': 'off',
+      'comma-dangle': ['warn', 'always-multiline'],
+      'dot-notation': 'off',
+      'eqeqeq': 'warn',
+      'curly': ['warn', 'all'],
+      'brace-style': ['warn'],
+      'prefer-arrow-callback': ['warn'],
+      'max-len': ['warn', 160],
+      'no-console': ['warn'],
+      'no-non-null-assertion': 'off',
+      'comma-spacing': ['error'],
+      'no-multi-spaces': ['warn', { ignoreEOLComments: true }],
+      'lines-between-class-members': [
+        'warn',
+        'always',
+        { exceptAfterSingleLine: true },
+      ],
+
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'off',
+        { args: 'none' },
+      ],
+    },
+  },
+];
